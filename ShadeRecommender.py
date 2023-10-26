5dff8c0999ac93d9087d72e0f33832691b693c1c
Matthias Yeo <lfj1234@gmail.com>
2023-10-26 20:34:04 +0800

    update

--- ShadeRecommender.py
+++ ShadeRecommender.py
@@ -1,34 +1,31 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import cv2
 import torch
 import segmentation_models_pytorch as smp
+from sklearn.cluster import KMeans
 
 class ShadeRecommender():
     def __init__(self, img_path):
         self.img_path = img_path
         self.segmodel = torch.load('./static/best_model.pth')
         self.output = self.predict_mask()
-        self.colour_rgb  = self.extract_avg_colour()
-        self.product_df = pd.read_excel('lips_loreal.xlsx')
-        self.product_dic = self.recommend_product()
+        self.binary_mask = self.store_binary_mask()
+        self.product_df = pd.read_excel('lipshades.xlsx')
+
         
     def predict_mask(self):
         '''
         takes in img_path and generates a predicted mask 
         '''
-    #     segmodel = torch.load('./best_model.pth')
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         BACKBONE = 'resnet34'
         self.segmodel.to(device)
         preprocess_input = smp.encoders.get_preprocessing_fn(encoder_name=BACKBONE, pretrained='imagenet')
 
         #helpers
-        # Define the image size you want
         IMG_SIZE = 256
 
         def resize_an_image(image_filename, new_size):
@@ -65,129 +62,100 @@ class ShadeRecommender():
 
         # Create a DataFrame with the single image path
         data = pd.DataFrame({'image_path': [self.img_path]})
-
         # Create an instance of the ImageDataset
         dataset = ImageDataset(data,preprocessing=preprocess_input)
-
         # Create a DataLoader with a batch size of 1 (since you have only one image)
         data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
-
         # access the single image as follows
         for batch in data_loader:
             image = batch
         output = self.segmodel(image)
-
         # Assuming output is the tensor
         output = output[0, 0].detach().cpu().numpy()  # Extract the single channel and convert to a NumPy array
-        
-#         # Visualize the predicted mask
-#         plt.imshow(output, cmap='jet', vmin=0, vmax=1)  # Use an appropriate colormap, vmin, and vmax
-# #         visualization_image_path = 'path_to_save_visualization.png'  # Specify the path and filename
-# #         plt.savefig(visualization_image_path)
-
 
         return output
-    
-    def extract_avg_colour(self):
+
+    def store_binary_mask(self):
         '''
-        function to reformat identified lip region back onto original image and extract average pixel colour
+        function to store binary mask used by other methods
         '''
-        original_image = cv2.imread(self.img_path)  # Replace with the path to your original image
-        
         # Define a threshold value
-        threshold = 0.3
+        threshold = 0.5
         
         # Create a binary mask for values greater than the threshold
         binary_mask = (self.output>threshold).astype(np.uint8)
 
-        # Resize the binary mask to match the dimensions of the original image
-        binary_mask_resized = cv2.resize(binary_mask, (original_image.shape[1], original_image.shape[0]))
-
-        mask = (binary_mask_resized == 1)
-
-        # Use the mask to extract the pixels from the segmented region
-        segmented_pixels = original_image[mask]
-
-        # Calculate the average color for the segmented pixels
-        average_color = np.mean(segmented_pixels, axis=0)
-
-
-
-        # convert to RGB 
-        avg_rgb = average_color[::-1]
-        
-        return avg_rgb
+        return binary_mask
     
-    def recommend_product(self):
-        target_color = self.colour_rgb
-        hex_lst = self.product_df['hex_colour'].to_list()
-        
-        # Function to convert hex to RGB
-        def hex_to_rgb(hex_color):
-            hex_color = hex_color.lstrip("#")
-            return np.array([int(hex_color[i:i+2], 16) for i in (0, 2, 4)])
+    def recommend_products(self):
+        '''
 
-        # Function to calculate Euclidean distance between two RGB colors
-        def color_distance(color1, color2):
-            return np.linalg.norm(color1 - color2)
+        '''
+        try:
+            original_image = cv2.imread(self.img_path) 
+
+            # Resize the binary mask to match the dimensions of the original image
+            binary_mask_resized = cv2.resize(self.binary_mask, (original_image.shape[1], original_image.shape[0]))
+
+            mask = (binary_mask_resized == 1)
+
+            # Use the mask to extract the pixels from the segmented region
+            segmented_pixels = original_image[mask]
+
+
+            colour_lst = []
+
+            # Calculate the average color for the segmented pixels]
+            average_color = np.mean(segmented_pixels, axis=0)
+            # convert to RGB 
+            avg_rgb = average_color[::-1]
+            colour_lst.append(avg_rgb)
+
+            #top 3 dominant colours
+            segmented_pixels_rgb = list(map(lambda row: [row[2], row[1], row[0]], segmented_pixels))
+            kmeans = KMeans(n_clusters=3)
+            kmeans.fit(segmented_pixels_rgb)
+
+            colour_lst.extend(kmeans.cluster_centers_)
+            target_colors = colour_lst
+            hex_lst = self.product_df['hexcode'].to_list()
+            
+            # Function to convert hex to RGB
+            def hex_to_rgb(hex_color):
+                return np.array([int(hex_color[i:i+2], 16) for i in (0, 2, 4)])
+
+            # Function to calculate Euclidean distance between two RGB colors
+            def colour_distance(color1, color2):
+                
+                return np.linalg.norm(color1 - color2)
+            
+            recommeded_product_ids = []
+            for colour in target_colors:
+                dist_lst = []
+                for i in range(len(hex_lst)):
+                    rgb = hex_to_rgb(str(hex_lst[i]))
+                    dist = colour_distance(colour, rgb)
+                    dist_lst.append(dist)
+                
+                product_idx = dist_lst.index(min(dist_lst))
+                product_id = self.product_df.loc[product_idx, "product_id"]
+                if product_id not in recommeded_product_ids:
+                    recommeded_product_ids.append(product_id)
+            return recommeded_product_ids          
         
-        dist_lst = []
-        for i in range(len(hex_lst)):
-            rgb = hex_to_rgb(hex_lst[i])
-            dist = color_distance(target_color, rgb)
-            dist_lst.append(dist)
-#         return dist_lst
-        
-        product_idx = dist_lst.index(min(dist_lst))
-        product_row = self.product_df.loc[product_idx]
-        product_dic = product_row.to_dict()
-        return product_dic
+        except Exception as e:
+            print(" Lips were not detected! Please upload another image! I will learn from this mistake!")
     
-#     def save_predicted_mask(self):
-#         '''
-#         generates the predicted mask for front-end
-#         '''
-        
-#         # Load the original image
-#         original_image = cv2.imread(self.img_path)  # Replace with the path to your original image
-        
-#         # Define a threshold value
-#         threshold = 0.3
-
-#         # Create a binary mask for values greater than the threshold
-#         binary_mask = (self.output>threshold).astype(np.uint8)
-
-#         # Resize the binary mask to match the dimensions of the original image
-#         binary_mask_resized = cv2.resize(binary_mask, (original_image.shape[1], original_image.shape[0]))
-
-#         # Create a copy of the original image
-#         overlay_image = original_image.copy()
-
-#         highlight_color = np.array([0, 240, 0])  # Green highlight in BGR format
-
-#         alpha = 0.4
-#         overlay_image[binary_mask_resized == 1] = (
-#             overlay_image[binary_mask_resized == 1] * (1 - alpha)
-#             + highlight_color * alpha
-#         )
-        
-#         cv2.imwrite('path_to_overlay_image2.jpeg', overlay_image)  # Replace with the desired output path
     def save_predicted_mask(self):
         '''
-        generates the predicted mask for front-end
+        front-end to run this specifically to generate and save generated mask
         '''
         
         # Load the original image
         original_image = cv2.imread(self.img_path)  # Replace with the path to your original image
-        
-        # Define a threshold value
-        threshold = 0.3
-
-        # Create a binary mask for values greater than the threshold
-        binary_mask = (self.output>threshold).astype(np.uint8)
 
         # Resize the binary mask to match the dimensions of the original image
-        binary_mask_resized = cv2.resize(binary_mask, (original_image.shape[1], original_image.shape[0]))
+        binary_mask_resized = cv2.resize(self.binary_mask, (original_image.shape[1], original_image.shape[0]))
 
         # Create a copy of the original image
         overlay_image = original_image.copy()
@@ -200,17 +168,14 @@ class ShadeRecommender():
             + highlight_color * alpha
         )
                 
-        #get filename
         filename = os.path.basename(self.img_path)
 
-        #generate new file path
         # Desired directory path
         directory_path = "./static/user_generated_masks"
 
         # Create the new filepath
         mask_filepath = os.path.join(directory_path, filename)
-        playground_path = os.path.join('static', 'playground', 'lipshadefinder', f'processed_{filename}')
+        
         cv2.imwrite(mask_filepath, overlay_image)
-        cv2.imwrite(playground_path, overlay_image)
-        return os.path.join('lipshadefinder', f'processed_{filename}')
+        return mask_filepath
 
