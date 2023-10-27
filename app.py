564b2360d7831f597249022bca2632ef041e360b
wchab <weicheng1723@gmail.com>
2023-10-27 23:11:39 +0800

    added loading

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file
 import os
-import base64
 import time
 import pandas as pd
-import concurrent.futures
-import asyncio
-from LipColorizer import LipColorizer
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file
 from flask_executor import Executor
 from UserHandler import UserHandler
-from ShadeRecommender import ShadeRecommender
 
 app = Flask(__name__, static_url_path='/static')
 executor = Executor(app)
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
-PRODUCTS_FILE = 'lips_loreal.xlsx'
-IMAGE_FOLDER = 'product_pictures'
 
 #initialise userhandler class as a centralised platform for front, back and user experience
 userhandler = UserHandler()
 
-# Function to check if the file extension is allowed
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
@@ -57,7 +49,14 @@ def get_products():
             return jsonify(recommended_line_dict)
         except:
             return {}
-            
+    selected_product = request.args.get('selected_product')
+    if selected_product:
+        destination_filename = os.path.join("static", "playground", "virtualtryon", "modified", f"{selected_product}.png")
+        hexcode = product_line_dict[selected_product]['hexcode']
+        image = userhandler.lipcolorizer.colorize_lips(f'#{hexcode}')
+        userhandler.lipcolorizer.saveImage(image, destination_filename)
+        time.sleep(2)
+        return jsonify({selected_product: product_line_dict[selected_product]})
     else:
         return jsonify(product_line_dict)
 
@@ -65,16 +64,6 @@ def get_products():
 def home():
     return render_template('home.html')
     
-def process_image(filename):
-    df = pd.read_excel('./static/lipshades.xlsx')
-    df['hexcode'] = df['hexcode'].map(lambda x: str(x))
-    hexcode_dict = dict(zip(df['product_id'], df['hexcode']))
-    for key in hexcode_dict.keys():
-        destination_filename = os.path.join("static", "playground", "virtualtryon", "modified", f"{key}.png")
-        image = userhandler.lipcolorizer.colorize_lips(f'#{hexcode_dict[key]}')
-        userhandler.lipcolorizer.saveImage(image, destination_filename)
-        print(f"{key} processed")
-
 @app.route('/virtualtryon', methods=['GET', 'POST'])
 def virtualtryon():
     if request.files:
@@ -89,7 +78,6 @@ def virtualtryon():
         if file and allowed_file(file.filename):
             file.save(userhandler.get_uploaded_virtualtryon_filename_path('upload.png'))
             userhandler.set_uploaded_virtualtryon_filename()
-            executor.submit(process_image(file.filename))
             return render_template('virtualtryon.html')
     else:
         return render_template('virtualtryon.html')
@@ -101,44 +89,6 @@ def upload_virtualtryon():
 @app.route('/upload_lipshadefinder', methods=['GET', 'POST'])
 def upload_lipshadefinder():
     return render_template('upload_lipshadefinder.html')
-
-def get_product_info(product_hexcode):
-    try:
-        df = pd.read_excel(os.path.join('static', PRODUCTS_FILE))
-
-        product_name = df[df['hex_colour'] == product_hexcode]['name'].iloc[0]
-        # product_colour = df[df['hex_colour'] == product_hexcode]['color'].iloc[0]
-        product_price = df[df['hex_colour'] == product_hexcode]['price'].iloc[0]
-        product_price_str = f"{product_price:.2f}"
-        product_id = df[df['hex_colour'] == product_hexcode]['prod_id'].iloc[0]
-
-        return product_name, product_price_str, product_id
-    
-    except Exception as e:
-        print(f"Error loading or searching in the Excel file: {e}")
-        return None, None, None
-
-# def get_product_colour(product_hexcode):
-#     try:
-#         df = pd.read_excel(PRODUCTS_FILE)
-
-#         result = df[df['hexcode'] == product_hexcode]['color'].iloc[0]
-
-#         return result
-#     except Exception as e:
-#         print(f"Error loading or searching in the Excel file: {e}")
-#         return None
-    
-# def get_product_id(product_hexcode):
-#     try:
-#         df = pd.read_excel(PRODUCTS_FILE)
-
-#         result = df[df['hexcode'] == product_hexcode]['product_id'].iloc[0]
-
-#         return result
-#     except Exception as e:
-#         print(f"Error loading or searching in the Excel file: {e}")
-#         return None  
 
 @app.route('/shadematched')
 def match_product():
